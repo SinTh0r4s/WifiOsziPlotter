633f57653370596135f1ac77e1e4eeb5eece1f1c
Johann Bernhardt <johann.bernhardt@tum.de>
2020-12-24 14:24:43 +0100

    added IDs to pc rx header

--- Headers.py
+++ Headers.py
@@ -13,6 +13,7 @@ class BeaconHeader:
     def __init__(self):
         self.resolution: int = 0
         self.channels: int = 0
+        self.beaconId: int = 0
         self.model: str = ""
         self.adc: str = ""
         self.frequency: int = 0
@@ -22,37 +23,39 @@ class BeaconHeader:
         self.port: int = 0
 
     def to_bytearray(self) -> bytearray:
-        buffer = bytearray(84)
+        buffer = bytearray(85)
         buffer[0:1] = magic_number.to_bytes(2, little_endian)
         buffer[2] = self.resolution
         buffer[3] = self.channels
-        buffer[4:33] = self.model.encode(encoding_utf8)
-        buffer[34:63] = self.adc.encode(encoding_utf8)
-        buffer[64:67] = self.frequency.to_bytes(4, little_endian)
-        buffer[68:71] = self.num_samples.to_bytes(4, little_endian)
+        buffer[4] = self.beaconId
+        buffer[5:34] = self.model.encode(encoding_utf8)
+        buffer[35:64] = self.adc.encode(encoding_utf8)
+        buffer[65:68] = self.frequency.to_bytes(4, little_endian)
+        buffer[69:72] = self.num_samples.to_bytes(4, little_endian)
         f = bytearray(struct.pack("f", self.sample_time))
         f.reverse()
-        buffer[72:75] = f
-        buffer[76:79] = self.v_ref.to_bytes(4, little_endian)
-        buffer[80:83] = self.port.to_bytes(4, little_endian)
+        buffer[73:76] = f
+        buffer[77:80] = self.v_ref.to_bytes(4, little_endian)
+        buffer[81:84] = self.port.to_bytes(4, little_endian)
         return buffer
 
     def from_bytearray(self, buffer: bytearray) -> bool:
-        if len(buffer) != 84:
+        if len(buffer) != 85:
             return False
         if buffer[0:1] != magic_number.to_bytes(2, little_endian):
             return False
         self.resolution = buffer[2]
         self.channels = buffer[3]
-        self.model = buffer[4:33].decode(encoding_utf8, ignore_errors)
-        self.adc = buffer[34:63].decode(encoding_utf8, ignore_errors)
-        self.frequency = int.from_bytes(buffer[64:67], little_endian)
-        self.num_samples = int.from_bytes(buffer[68:71], little_endian)
-        b = buffer[72:75]
+        self.beaconId = buffer[4]
+        self.model = buffer[5:34].decode(encoding_utf8, ignore_errors)
+        self.adc = buffer[35:64].decode(encoding_utf8, ignore_errors)
+        self.frequency = int.from_bytes(buffer[65:68], little_endian)
+        self.num_samples = int.from_bytes(buffer[69:72], little_endian)
+        b = buffer[73:76]
         b.reverse()
         self.sample_time = struct.unpack("f", b)
-        self.v_ref = int.from_bytes(buffer[76:79], little_endian)
-        self.port = int.from_bytes(buffer[80:83], little_endian)
+        self.v_ref = int.from_bytes(buffer[77:80], little_endian)
+        self.port = int.from_bytes(buffer[81:84], little_endian)
         return True
 
 
@@ -118,6 +121,7 @@ class SampleTransmissionHeader:
     def __init__(self):
         self.frame_id: int = 0
         self.num_frames: int = 0
+        self.transmission_group_id: int = 0
         self.resolution: int = 0
         self.channels: int = 0
         self.frequency: int = 0
@@ -126,37 +130,43 @@ class SampleTransmissionHeader:
         self.payload: bytearray = bytearray(0)
 
     def to_bytearray(self) -> bytearray:
-        buffer = bytearray(18 + len(self.payload))
+        buffer = bytearray(19 + len(self.payload))
         buffer[0:1] = magic_number.to_bytes(2, little_endian)
         buffer[2] = self.frame_id
         buffer[3] = self.num_frames
-        buffer[4] = self.resolution
-        buffer[5] = self.channels
-        buffer[6:9] = self.frequency.to_bytes(4, little_endian)
-        buffer[10:13] = self.v_ref.to_bytes(4, little_endian)
-        buffer[14:17] = self.num_samples.to_bytes(4, little_endian)
-        buffer[18:] = self.payload
+        buffer[4] = self.transmission_group_id
+        buffer[5] = self.resolution
+        buffer[6] = self.channels
+        buffer[7:10] = self.frequency.to_bytes(4, little_endian)
+        buffer[11:14] = self.v_ref.to_bytes(4, little_endian)
+        buffer[15:18] = self.num_samples.to_bytes(4, little_endian)
+        buffer[19:] = self.payload
         return buffer
 
     def from_bytearray(self, buffer: bytearray) -> bool:
-        if len(buffer) < 18:
+        if len(buffer) < 19:
             return False
         if buffer[0:1] != magic_number.to_bytes(2, little_endian):
             return False
         self.frame_id = buffer[2]
         self.num_frames = buffer[3]
-        self.resolution = buffer[4]
-        self.channels = buffer[5]
-        self.frequency = int.from_bytes(buffer[6:9], little_endian)
-        self.v_ref = int.from_bytes(buffer[10:13], little_endian)
-        self.num_samples = int.from_bytes(buffer[14:17], little_endian)
-        if len(buffer) != 18 + self.num_samples * get_bytes_per_sample(self.resolution):
+        if self.frame_id >= self.num_frames:
             return False
-        self.payload = buffer[18:]
+        self.transmission_group_id = buffer[4]
+        self.resolution = buffer[5]
+        self.channels = buffer[6]
+        self.frequency = int.from_bytes(buffer[7:10], little_endian)
+        self.v_ref = int.from_bytes(buffer[11:14], little_endian)
+        self.num_samples = int.from_bytes(buffer[15:18], little_endian)
+        if len(buffer) != 19 + self.num_samples * get_bytes_per_sample(self.resolution):
+            return False
+        self.payload = buffer[19:]
         return True
 
-    def compare(self, other):
+    def same_transmission_group(self, other):
         if self.num_frames != other.num_frames:
+            return False
+        if self.transmission_group_id != other.transmission_group_id:
             return False
         if self.resolution != other.resolution:
             return False
