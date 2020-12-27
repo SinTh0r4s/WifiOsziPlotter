eceeb50d5edf477ae6aad4bd49d24ba5b2aabcaa
Johann Bernhardt <johann.bernhardt@tum.de>
2020-12-28 00:36:49 +0100

    typing fix

--- osziplotter/modelcontroller/PlotInfo.py
+++ osziplotter/modelcontroller/PlotInfo.py
@@ -7,7 +7,7 @@ class PlotInfo:
 
     def __init__(self):
         self.timestamp = time()
-        self.channels: Dict[int, np.array] = {}
+        self.channels: Dict[int, np.ndarray] = {}
         self.v_ref: int = 3300
         self.resolution: int = 0
         self.frequency: int = 0
