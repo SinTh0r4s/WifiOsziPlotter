2f1d01f0028ea598f9cd8e4521df53c7031aab57
Johann Bernhardt <johann.bernhardt@tum.de>
2020-12-29 15:13:30 +0100

    implemented export to matlab function

--- osziplotter/view/ExportWidget.py
+++ osziplotter/view/ExportWidget.py
@@ -1,8 +1,9 @@
 from osziplotter.modelcontroller.PlotEvents import PlotEvents
 from osziplotter.modelcontroller.PlotInfo import PlotInfo
 
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QPushButton, QFileDialog
 from typing import Dict
+from scipy.io import savemat
 
 
 class ExportWidget(QPushButton, PlotEvents):
@@ -10,9 +11,14 @@ class ExportWidget(QPushButton, PlotEvents):
     def __init__(self, *args, **kwargs) -> None:
         super(ExportWidget, self).__init__("Export to Matlab", *args, **kwargs)
         self.clicked.connect(self._export)
+        self.setDisabled(True)
+        self._current_plot: PlotInfo = None
 
     def _export(self) -> None:
-        pass
+        filename, _ = QFileDialog().getSaveFileName(self, "Save plot as ...", filter="Matlab Data File (*.mat)")
+        if filename != "":
+            savemat(filename, self._current_plot.__dict__)
 
     def update_plot(self, plots: Dict[float, PlotInfo], visible_plot: PlotInfo = None) -> None:
-        pass
+        self._current_plot = visible_plot
+        self.setDisabled(visible_plot is None)
